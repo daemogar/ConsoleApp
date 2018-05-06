21c0abbf29f491ae7fdd59b0ce296ba3c4f45e60
Paul Irwin <daemogar@gmail.com>
2018-05-05 20:44:12 -0400

    Add project files.

---
+++ CardCombatGameServer/Hubs/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CardCombatGameServer.Hubs
+{
+  public class GameHub : Hub
+  {
+    public Task Send(string message)
+    {
+      return Clients.All.SendAsync("Send", message);
+    }
+  }
+}
